23657827c72d9d796fa2c5e41656836dad2e5f55
zuldare <jaime.hernandezortiz@gmail.com>
2022-06-22 22:40:33 +0200

    First commit

---
+++ src/test/java/jj/hernandez/learning/LearningApplicationTests.java
@@ -0,0 +1,13 @@
+package jj.hernandez.learning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearningApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
